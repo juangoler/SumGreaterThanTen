6131c2da532e0e6ba9201b95617af33e86871e53
Juan Goler <juanandreigoler@gmail.com>
2024-09-09 16:35:37 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int valor1, valor2, soma;
+   
+   printf("Digite o primeiro valor: ");
+   scanf("%d", &valor1);
+   
+   printf("Digite o segundo valor: ");
+   scanf("%d", &valor2);
+   
+   soma = valor1 + valor2;
+   
+   if (soma > 10) {
+
+   printf("A soma dos valores é: %d\n", soma);
+   
+   }
+
+   return 0;
+}
